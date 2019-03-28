89d244b87c1cd11661dc19ccaefc6c39cb8f1175
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-03-28 16:35:36 -0700

    Working on min-heap to max-heap

---
+++ d03/ex03/saveTheSequoia.c
@@ -0,0 +1,13 @@
+#include "header.h"
+
+void	saveTheSequoia(struct s_node **root) {
+	struct s_node	*head;
+
+	if (!root || !*root)
+		return ;
+	head = *root;
+	if (head->left && head->left->value > head->value) {
+		tmp = head;
+
+	}
+}

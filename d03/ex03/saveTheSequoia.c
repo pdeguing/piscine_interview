f8d6f2d1b9361dab955abd85f8add35e4a01976c
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-03-29 11:55:35 -0700

    Transforming min-heap into max-heap with just swapping nodes

--- d03/ex03/saveTheSequoia.c
+++ d03/ex03/saveTheSequoia.c
@@ -1,13 +1,49 @@
 #include "header.h"
+#include <stdio.h>
+
+void	swapLeft(struct s_node **root, struct s_node **left) {
+	struct s_node	*tmp;
+
+	tmp = *root;
+	*root = *left;
+	*left = tmp;
+	tmp = (*root)->right;
+	(*root)->right = (*left)->right;
+	(*left)->right = tmp;
+	tmp = (*root)->left;
+	(*root)->left = (*left)->left;
+	(*left)->left = tmp;
+}
+
+void	swapRight(struct s_node **root, struct s_node **right) {
+	struct s_node	*tmp;
+
+	tmp = *root;
+	*root = *right;
+	*right = tmp;
+	tmp = (*root)->left;
+	(*root)->left = (*right)->left;
+	(*right)->left = tmp;
+	tmp = (*root)->right;
+	(*root)->right = (*right)->right;
+	(*right)->right = tmp;
+}
 
 void	saveTheSequoia(struct s_node **root) {
-	struct s_node	*head;
-
 	if (!root || !*root)
 		return ;
-	head = *root;
-	if (head->left && head->left->value > head->value) {
-		tmp = head;
-
+	saveTheSequoia(&((*root)->left));
+	if ((*root)->left && (*root)->value < (*root)->left->value) {
+		swapLeft(root, &((*root)->left));
+		//saveTheSequoia(&((*root)->left));
+		//saveTheSequoia(&((*root)->right));
+		saveTheSequoia(root);
+	}
+	saveTheSequoia(&((*root)->right));
+	if ((*root)->right && (*root)->value < (*root)->right->value) {
+		swapRight(root, &((*root)->right));
+		//saveTheSequoia(&((*root)->left));
+		//saveTheSequoia(&((*root)->right));
+		saveTheSequoia(root);
 	}
 }

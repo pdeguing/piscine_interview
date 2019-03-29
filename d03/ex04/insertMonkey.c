8c150eb900ca0a9435d26d08baad699ba0cfa7de
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-03-29 14:58:26 -0700

    Working on max-heap insertion

---
+++ d03/ex04/insertMonkey.c
@@ -0,0 +1,59 @@
+#include "header.h"
+
+void	swapLeft(struct s_node **root, struct s_node **left) {
+	struct s_node	*tmp;
+
+	tmp = *root;
+	*root = *left;
+	*left = tmp;
+	tmp = (*root)->right;
+	(*root)->right = (*left)->right;
+	(*left)->right = tmp;
+	tmp = (*root)->left;
+	(*root)->left = (*left)->left;
+	(*left)->left = tmp;
+}
+
+void	swapRight(struct s_node **root, struct s_node **right) {
+	struct s_node	*tmp;
+
+	tmp = *root;
+	*root = *right;
+	*right = tmp;
+	tmp = (*root)->left;
+	(*root)->left = (*right)->left;
+	(*right)->left = tmp;
+	tmp = (*root)->right;
+	(*root)->right = (*right)->right;
+	(*right)->right = tmp;
+}
+
+void	saveTheSequoia(struct s_node **root) {
+	if (!root || !*root)
+		return ;
+	saveTheSequoia(&((*root)->left));
+	if ((*root)->left && (*root)->value < (*root)->left->value) {
+		swapLeft(root, &((*root)->left));
+		//saveTheSequoia(&((*root)->left));
+		//saveTheSequoia(&((*root)->right));
+		saveTheSequoia(root);
+	}
+	saveTheSequoia(&((*root)->right));
+	if ((*root)->right && (*root)->value < (*root)->right->value) {
+		swapRight(root, &((*root)->right));
+		//saveTheSequoia(&((*root)->left));
+		//saveTheSequoia(&((*root)->right));
+		saveTheSequoia(root);
+	}
+}
+
+void	insertMonkey(struct s_node **root, struct s_node *monkey) {
+
+	if (!*root) {
+		*root = monkey;
+		return ;
+	}
+	monkey->left = *root;
+	*root = monkey;
+	saveTheSequoia(root);
+}
